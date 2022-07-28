3160c951f761dfe093457a63d0a5b654cd8f627c
Joxx0r <Joxxor@gmail.com>
2022-07-28 15:33:50 +0200

    Added basic window type definitions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+type c_int = i32;
+type c_uint = u32;
+type HANDLE = PVOID;
+type HBRUSH = HANDLE;
+type HCURSOR = HICON;
+type HICON = HANDLE;
+type HINSTANCE = HANDLE;
+type HWND = HANDLE;
+type LONG_PTR = isize;
+type LPARAM = LONG_PTR;
+type LPCWSTR = *const WCHAR;
+type LRESULT = LONG_PTR;
+type PVOID = *mut core::ffi::c_void;
+type UINT = c_uint;
+type UINT_PTR = usize;
+type WCHAR = wchar_t;
+type wchar_t = u16;
+type WPARAM = UINT_PTR;
+
+
+type WNDPROC = Option<
+  unsafe extern "system" fn(
+    hwnd: HWND,
+    uMsg: UINT,
+    wParam: WPARAM,
+    lParam: LPARAM,
+  ) -> LRESULT,
+>;
+
+#[repr(C)]
+pub struct WNDCLASSW {
+  style: UINT,
+  lpfnWndProc: WNDPROC,
+  cbClsExtra: c_int,
+  cbWndExtra: c_int,
+  hInstance: HINSTANCE,
+  hIcon: HICON,
+  hCursor: HCURSOR,
+  hbrBackground: HBRUSH,
+  lpszMenuName: LPCWSTR,
+  lpszClassName: LPCWSTR,
+}
+
 fn main() {
     println!("Hello, world!");
 }
